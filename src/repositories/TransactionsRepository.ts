39a78952eec686906920fe88fde7c4617d8244d1
Nilo Neregato <neregato.nilo@gmail.com>
2020-04-15 00:33:56 +0200

    fix challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acc, currentValue) => {
+      if (currentValue.type === 'income') {
+        return acc + currentValue.value;
+      }
+      return acc + 0;
+    }, 0);
+
+    const outcome = this.transactions.reduce((acc, currentValue) => {
+      if (currentValue.type === 'outcome') {
+        return acc + currentValue.value;
+      }
+      return acc + 0;
+    }, 0);
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
